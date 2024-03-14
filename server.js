44f3e29663dbb5e335971088dd46f497bbf8ca23
Pradyumna-1 <pradyumnakumar35860@gmail.com>
2024-03-14 22:04:09 +0530

    add the view part

--- server.js
+++ server.js
@@ -1,56 +1,29 @@
-// const express = require("express");
-// const { connectToMongoDB } = require("./connect");
-// const urlRoute = require("./routes/url");
-// const URL = require("./models/url");
-
-// const app = express();
-// const PORT = 8001;
-
-// connectToMongoDB(
-//   "mongodb+srv://pradyumnakumarnaik1:MAPLEhATy664174B@cluster0.bwcnfjb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-// ).then(() => console.log("Mongodb connected"));
-
-// app.use(express.json());
-
-// app.use("/url", urlRoute);
-
-// app.get("/:shortId", async (req, res) => {
-//   const shortId = req.params.shortId;
-//   const entry = await URL.findOneAndUpdate(
-//     {
-//       shortId,
-//     },
-//     {
-//       $push: {
-//         visitHistory: {
-//           timestamp: Date.now(),
-//         },
-//       },
-//     }
-//   );
-//   res.redirect(entry.redirectURL);
-// });
-
-// app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
 const express = require("express");
 const mongoose = require("mongoose"); // Import mongoose
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
 const URL = require("./models/url");
+const staticRoute = require("./routes/staticRouter");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
 // Connect to the local MongoDB database
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
-  .then(() => console.log("Local MongoDB connected"))
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Local MongoDB connection error:", err));
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
